def1f8ed06d8e7c083ed435893aefef5f980bd5f
kazike19 <kazike.mv@gmail.com>
2021-01-06 00:03:44 -0600

    Update desing.js

--- src/js/desing.js
+++ src/js/desing.js
@@ -15,9 +15,9 @@ function iniciar(){
 	tabla.appendChild(lista);
 }
 
-function AgregarFinal(){
+/* 	function AgregarFinal(){
 
-	// hacer dos if con lineas, para el espacio y para sin el, omitir el for
+	 hacer dos if con lineas, para el espacio y para sin el, omitir el for
 	var valor="____________";
 	for (var j = 0; j < 3; j++) {
 	celda=document.createElement("td");
@@ -31,5 +31,40 @@ function AgregarFinal(){
 	lista=document.createElement("tr")//se crea
 			tabla.appendChild(lista);
 
-}	
+}*/
 
+function final(){
+	// hacer dos if con lineas, para el espacio y para sin el, omitir el for
+	var a,b,c;
+	a=parseInt(hexa.tirada1.value,10);
+	b=parseInt(hexa.tirada2.value,10);
+	c=parseInt(hexa.tirada3.value,10);
+
+	var aux=a+b+c;
+	var valor8="____    ____";
+	var valor7="_________";
+	if(aux==7){
+	celda=document.createElement("td");
+	texto=document.createTextNode(valor7);
+	celda.appendChild(texto);
+	if (lista.hasChildNodes())
+		lista.insertBefore(celda,lista.childNodes[0]);
+	else
+		lista.appendChild(celda);
+
+		}
+
+	if(aux==8){
+			celda=document.createElement("td");
+			texto=document.createTextNode(valor8);
+			celda.appendChild(texto);
+			if (lista.hasChildNodes())
+				lista.insertBefore(celda,lista.childNodes[0]);
+			else
+				lista.appendChild(celda);
+		}
+
+	lista=document.createElement("tr")//se crea
+	tabla.appendChild(lista);
+}
+	
