c5a1f99539e2810b9cf24c14adb780535a2e8e0e
kazike19 <kazike.mv@gmail.com>
2021-01-05 22:15:32 -0600

    Update desing.js

--- src/js/desing.js
+++ src/js/desing.js
@@ -1,3 +1,5 @@
+var tabla,lista,celda,texto,supercel;
+
 function onClickAgregarInText(){ 
    var padre = document.getElementsByClassName("Tiradas")[0];
    
@@ -6,3 +8,28 @@ function onClickAgregarInText(){
    input.className = 'inputText';
    padre.appendChild(input);
 }
+
+function iniciar(){
+	tabla=document.getElementById("tabla1");
+	lista=document.createElement("tr")//se crea
+	tabla.appendChild(lista);
+}
+
+function AgregarFinal(){
+
+	// hacer dos if con lineas, para el espacio y para sin el, omitir el for
+	var valor="____________";
+	for (var j = 0; j < 3; j++) {
+	celda=document.createElement("td");
+	texto=document.createTextNode(valor);
+	celda.appendChild(texto);
+	if (lista.hasChildNodes())
+			    lista.insertBefore(celda,lista.childNodes[0]);
+			else
+			    lista.appendChild(celda);
+	}
+	lista=document.createElement("tr")//se crea
+			tabla.appendChild(lista);
+
+}	
+
